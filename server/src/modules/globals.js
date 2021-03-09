e52d59023ad4cd5537cf93d1458a1b39bec19a33
beedude <suneettipirneni@icloud.com>
2021-03-09 17:23:16 -0500

    Initial Commit

--- server/src/modules/globals.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import pino from 'pino';
-
-// eslint-disable-next-line no-underscore-dangle
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const logger = pino();
-
-export { __dirname, logger };
