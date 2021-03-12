a75bd40bafe1abb94024eee0d18a68d2d4b2df3f
Elbert <etse168@gmail.com>
2021-03-12 11:45:34 -0500

    Wrote up structure of card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+// Importing react.
+import React from 'react';
+// Importing scss.
+import '../scss/card.scss';
+// Importing PropTypes, defines the props for this component.
+import PropTypes from 'prop-types';
+
+function Card({ children }) {
+  return (
+        <div
+          className = 'card'
+        >
+            {children}
+        </div>
+  );
+}
+
+Card.PropTypes = {
+  children: PropTypes.element,
+};
+
+Card.defaultProps = {
+  children: [],
+};
+
+export default Card;
