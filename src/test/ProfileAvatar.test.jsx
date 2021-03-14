48901f55c80ed2b4cbfc5bbf4b840a1cb4a1d1c2
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-03-13 21:24:14 -0500

    Finalize tests

--- src/test/ProfileAvatar.test.jsx
+++ src/test/ProfileAvatar.test.jsx
@@ -36,4 +36,5 @@ it('renders profile avatar properly given data', () => {
     avatar.imgURL = 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80';
     render(<ProfileAvatar avatarData={avatar} />, container);
   });
+  expect(container.querySelector('img')).toBeTruthy();
 });
