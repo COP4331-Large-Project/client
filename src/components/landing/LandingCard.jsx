f25a926d8da7ce716d8b75f1ab2143b082e52324
ctcuff <dev.ctcuff@gmail.com>
2021-03-20 16:33:01 -0400

    Added register card component

--- src/components/landing/LandingCard.jsx
+++ src/components/landing/LandingCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../scss/landing.scss';
 
-function LandingCard({ title, children }) {
+function LandingCard({ title, children, className }) {
   return (
-    <div className="landing-card">
+    <div className={`landing-card ${className}`}>
       <h1>{title}</h1>
       <div className="landing-card-content-container">
         {children}
@@ -15,11 +15,13 @@ function LandingCard({ title, children }) {
 
 LandingCard.defaultProps = {
   children: <div className="" />,
+  className: '',
 };
 
 LandingCard.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 export default LandingCard;
