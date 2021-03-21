a3b28f94cec98ac56268a098870fc61e23db5cf9
Elbert <etse168@gmail.com>
2021-03-21 15:16:21 -0400

    Reverted landing page card back to default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,22 @@ import '../scss/button.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './landing/LandingPage.jsx';
-// import TextInput from './TextInput.jsx';
-// import Button from './Button.jsx';
-// import LandingCard from './landing/LandingCard.jsx';
+import TextInput from './TextInput.jsx';
+import Button from './Button.jsx';
+import LandingCard from './landing/LandingCard.jsx';
 // Leaving GroupLogin commented out.
-import GroupLogin from './landing/GroupLogin.jsx';
+// import GroupLogin from './landing/GroupLogin.jsx';
 
 function App() {
   // eslint-disable-next-line no-unused-vars
   const [card, setCard] = useState(
-    // <LandingCard title="Title">
-    //   <div className="card-form">
-    //     <TextInput />
-    //     <TextInput />
-    //     <Button>Click Me</Button>
-    //   </div>
-    // </LandingCard>,
-    <GroupLogin></GroupLogin>,
+    <LandingCard title="Title">
+      <div className="card-form">
+        <TextInput />
+        <TextInput />
+        <Button>Click Me</Button>
+      </div>
+    </LandingCard>,
   );
 
   return (
