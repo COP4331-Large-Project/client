8f6d91e9a9bd64757e812fdfb183775f01d62b9a
Elbert <etse168@gmail.com>
2021-03-29 21:53:29 -0400

    Renamed input names.

--- src/components/landing/RegisterCard.jsx
+++ src/components/landing/RegisterCard.jsx
@@ -18,25 +18,25 @@ function RegisterCard({ switchCard }) {
   async function onSubmit(event) {
     event.preventDefault();
     const formInfo = new FormData(event.target);
-    const data = Object.fromEntries(formInfo.entries());
+    const data = Object.fromEntries(formInfo);
 
     try {
       // Checking if passwords match
-      if (data.Password !== data.ConfirmPassword) throw (new Error('Passwords do not match.'));
-      if (isTrimmedEmpty(data.FirstName)) throw (new Error('First name input required.'));
-      if (isTrimmedEmpty(data.LastName)) throw (new Error('Last name input required.'));
-      if (isTrimmedEmpty(data.Username)) throw (new Error('Username input required.'));
-      if (isTrimmedEmpty(data.Email)) throw (new Error('Email input required.'));
-      if (isTrimmedEmpty(data.Password)) throw (new Error('Password input required.'));
-      if (isTrimmedEmpty(data.ConfirmPassword)) throw (new Error('Confirm password input required.'));
+      if (data.password !== data.confirmPassword) throw (new Error('Passwords do not match.'));
+      if (isTrimmedEmpty(data.firstName)) throw (new Error('First name input required.'));
+      if (isTrimmedEmpty(data.lastName)) throw (new Error('Last name input required.'));
+      if (isTrimmedEmpty(data.username)) throw (new Error('Username input required.'));
+      if (isTrimmedEmpty(data.email)) throw (new Error('Email input required.'));
+      if (isTrimmedEmpty(data.password)) throw (new Error('Password input required.'));
+      if (isTrimmedEmpty(data.confirmPassword)) throw (new Error('Confirm password input required.'));
 
       // Calling register API
       await API.register({
-        firstName: data.FirstName,
-        lastName: data.LastName,
-        email: data.Email,
-        username: data.Username,
-        password: data.Password,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        username: data.username,
+        password: data.password,
       });
     } catch (e) {
       setError(e.message);
@@ -50,13 +50,13 @@ function RegisterCard({ switchCard }) {
     <LandingCard title="Register" error={err}>
       <form onSubmit={onSubmit}>
         <div className="input-group">
-          <TextInput placeHolder="First name" name="FirstName" required/>
-          <TextInput placeHolder="Last name" name="LastName" required/>
+          <TextInput placeHolder="First name" name="firstName" required/>
+          <TextInput placeHolder="Last name" name="lastName" required/>
         </div>
-        <TextInput placeHolder="Username" name="Username" required/>
-        <TextInput placeHolder="Email" type="email" name="Email" required/>
-        <TextInput placeHolder="Password" type="password" name="Password" required/>
-        <TextInput placeHolder="Confirm password" type="password" name="ConfirmPassword" required/>
+        <TextInput placeHolder="Username" name="username" required/>
+        <TextInput placeHolder="Email" type="email" name="email" required/>
+        <TextInput placeHolder="Password" type="password" name="password" required/>
+        <TextInput placeHolder="Confirm password" type="password" name="confirmPassword" required/>
         <Button className="btn-submit" type="submit">Register</Button>
         <Button className="btn-link" onClick={() => switchCard('login')}>
           Back to login
