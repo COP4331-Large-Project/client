0fd66ffdd4ddc43067a7405fb14ace23a714e4c8
ctcuff <dev.ctcuff@gmail.com>
2021-03-31 20:19:42 -0400

    Create skeleton for main layout

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,19 @@
+import 'antd/dist/antd.css';
+import '../scss/main-page.scss';
+import { Layout } from 'antd';
 import React from 'react';
+
+const { Sider, Content, Header } = Layout;
 
 function MainPage() {
   return (
-    <div/>
+    <Layout className="main-page">
+      <Header></Header>
+      <Layout>
+        <Sider theme="light"></Sider>
+        <Content></Content>
+      </Layout>
+    </Layout>
   );
 }
 
