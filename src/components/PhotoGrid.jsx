423f289662726cd17ed4ee30d08672c9cf01c449
suneettipirneni <suneettipirneni@icloud.com>
2021-04-04 19:06:23 -0400

    Add animations

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 import '../scss/photo-grid.scss';
 import PhotoThumbnail from './PhotoThumbnail.jsx';
 
 function PhotoGrid({ photos }) {
   return (
+    <motion.div>
         <div className="photo-grid">
             {photos.map(photo => (
                 <PhotoThumbnail key={photo} src={photo} />
             ))}
         </div>
+      </motion.div>
   );
 }
 
--- src/components/PhotoThumbnail.jsx
+++ src/components/PhotoThumbnail.jsx
@@ -1,9 +1,30 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 
 function PhotoThumbnail({ src }) {
   return (
-        <img className="photo-thumbnail" src={src} />
+    <motion.div
+    initial={{ y: 1000 }}
+    animate={{ y: 0 }}
+    transition={{
+      duration: 1, type: 'spring', bounce: 0.1,
+    }}
+    whileHover={{
+      scale: 1.1,
+      transition: {
+        duration: 0.4, type: 'spring', bounce: 0.5,
+      },
+    }}
+    whileTap={{
+      scale: 1.1,
+      transition: {
+        duration: 0.4, type: 'spring', bounce: 0.5,
+      },
+    }}>
+      <img className="photo-thumbnail" src={src} />
+    </motion.div>
   );
 }
 
