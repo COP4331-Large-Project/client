da0f68ac47175d9369b8e8967d446731dd821b36
Elbert <etse168@gmail.com>
2021-04-05 14:35:16 -0400

    Converted group code login component to password reset

--- src/components/landing/ForgotPassword.jsx
+++ src/components/landing/ForgotPassword.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { Tooltip } from 'antd';
-import { BsQuestionCircle } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 import LandingCard from './LandingCard.jsx';
 import TextInput from '../TextInput.jsx';
@@ -14,29 +12,24 @@ function ForgotPassword({ switchCard }) {
   let input;
   const [err, setError] = useState(null);
 
-  function login(event) {
+  function sendResetEmail(event) {
     event.preventDefault();
     // TODO: Make login request
     setError('Not Implemented');
   }
 
   return (
-    <LandingCard error={err}>
+    <LandingCard title='Password reset' error={err}>
       <form>
         <TextInput
-          placeHolder="Group Code"
+          placeHolder="johndoe@email.com"
           // eslint-disable-next-line no-unused-vars
           onChange={(c) => { input = c; }}
         />
-        <Button onClick={login}>
-          Enter Code
+        <Button onClick={sendResetEmail}>
+          Send Reset Email
         </Button>
         <Button className="btn-link" onClick={() => switchCard('login')}>Back To Login</Button>
-        <Tooltip placement='bottom' title='A group code is used to log in anonymously as a guest. No groups will be saved.'>
-          <h4 className='landing-hint'>
-            What is a group code <BsQuestionCircle/>
-          </h4>
-        </Tooltip>
       </form>
     </LandingCard>
   );
