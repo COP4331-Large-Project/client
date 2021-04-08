9e342cd9d41c9fd864eef52b4ee34f63d3ae9f51
ctcuff <dev.ctcuff@gmail.com>
2021-04-07 22:53:47 -0400

    Added group list component

---
+++ src/components/dashboard/GroupList.jsx
@@ -0,0 +1,64 @@
+import '../../scss/group-list.scss';
+import React from 'react';
+import { Avatar, List } from 'antd';
+import PropTypes from 'prop-types';
+
+function GroupList({ groups, onGroupClick }) {
+  const getMemberText = members => {
+    if (members === 1) {
+      return `${members} member`;
+    }
+
+    return `${members} members`;
+  };
+
+  const renderGroupImage = (imageURL, title) => (
+    <Avatar src={imageURL} size={54} alt={title}>
+      {title[0]}
+    </Avatar>
+  );
+
+  const renderListItem = ({ title, imageURL, members }, index) => (
+    <List.Item onClick={() => onGroupClick(groups[index])} title={title}>
+      <List.Item.Meta
+        avatar={renderGroupImage(imageURL, title)}
+        title={title}
+        description={getMemberText(members)}
+      />
+    </List.Item>
+  );
+
+  return (
+    <div className="group-list">
+      <List
+        dataSource={groups}
+        header={<p className="list-header">Your Groups</p>}
+        locale={{
+          emptyText: `
+            Looks like you’re not a member of any groups yet. Click the
+            "Create Group" button to create a new group or click the
+            "Join Group" button to join a group.
+          `,
+        }}
+        renderItem={renderListItem}
+      />
+    </div>
+  );
+}
+
+GroupList.propTypes = {
+  groups: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      imageURL: PropTypes.string,
+      members: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  onGroupClick: PropTypes.func,
+};
+
+GroupList.defaultProps = {
+  onGroupClick: () => {},
+};
+
+export default GroupList;
--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -3,14 +3,47 @@ import React from 'react';
 import { Layout } from 'antd';
 import Card from '../Card.jsx';
 import InviteArea from './InviteArea.jsx';
+import GroupList from './GroupList.jsx';
 
 const { Sider } = Layout;
+
+const groups = [
+  {
+    title: 'Group TBA',
+    imageURL: null,
+    members: 103,
+  },
+  {
+    title: 'Socks n Sandals',
+    imageURL:
+      'https://images.unsplash.com/photo-1617450599731-0ec86e189589?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+    members: 592,
+  },
+  {
+    title: 'Architecture',
+    imageURL:
+      'https://images.unsplash.com/photo-1617538781052-b49b1bc7cbe1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1866&q=80',
+    members: 900,
+  },
+  {
+    title: 'JavaScript Junkies',
+    imageURL:
+      'https://images.unsplash.com/photo-1617541224621-c6dbd1bb5bbb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+    members: 1,
+  },
+  {
+    title: 'This is a group with a title that should show ellipses',
+    imageURL: 'https://unsplash.com/photos/IGfIGP5ONV0',
+    members: 420,
+  },
+];
 
 function Sidebar() {
   return (
     <Sider theme="light" className="sidebar">
       <Card className="sidebar-card">
         <InviteArea inviteCode="xJwY394p" />
+        <GroupList groups={groups} />
       </Card>
     </Sider>
   );
