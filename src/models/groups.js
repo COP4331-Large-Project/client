31b42830d1c538d413525625b0f9995130894208
Elbert <etse168@gmail.com>
2021-04-14 10:42:22 -0400

    Added images to dummy group list

--- src/models/groups.js
+++ src/models/groups.js
@@ -40,12 +40,6 @@ const groups = [
       },
     ],
     images: [
-      {
-        fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        creator: 'string',
-        dateUploaded: '2021-04-14T14:01:41.058Z',
-        URL: 'string',
-      },
     ],
     thumbnail: {
       fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
@@ -92,7 +86,7 @@ const groups = [
         fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         creator: 'string',
         dateUploaded: '2021-04-14T14:01:41.058Z',
-        URL: 'string',
+        URL: 'https://images.unsplash.com/photo-1617450599731-0ec86e189589?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
       },
     ],
     thumbnail: {
@@ -148,7 +142,19 @@ const groups = [
         fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         creator: 'string',
         dateUploaded: '2021-04-14T14:01:41.058Z',
-        URL: 'string',
+        URL: 'https://images.unsplash.com/photo-1617541224621-c6dbd1bb5bbb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+      },
+      {
+        fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        creator: 'string',
+        dateUploaded: '2021-04-14T14:01:41.058Z',
+        URL: 'https://images.unsplash.com/photo-1617547138068-230e9714712e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+      },
+      {
+        fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        creator: 'string',
+        dateUploaded: '2021-04-14T14:01:41.058Z',
+        URL: 'https://images.unsplash.com/photo-1617330070571-60434437a26c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80',
       },
     ],
     thumbnail: {
@@ -204,7 +210,7 @@ const groups = [
         fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         creator: 'string',
         dateUploaded: '2021-04-14T14:01:41.058Z',
-        URL: 'string',
+        URL: 'https://images.unsplash.com/photo-1617504546842-5768342f1c46?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2102&q=80',
       },
     ],
     thumbnail: {
@@ -260,7 +266,7 @@ const groups = [
         fileName: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         creator: 'string',
         dateUploaded: '2021-04-14T14:01:41.058Z',
-        URL: 'string',
+        URL: 'another-bad-url',
       },
     ],
     thumbnail: {
