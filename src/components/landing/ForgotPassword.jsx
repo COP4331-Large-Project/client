53ca87903ab839001b7ee0ea1c3b8cb696855b39
Elbert <etse168@gmail.com>
2021-04-18 21:44:44 -0400

    Moved callback call to form submit rather than button click

--- src/components/landing/ForgotPassword.jsx
+++ src/components/landing/ForgotPassword.jsx
@@ -4,29 +4,31 @@ import LandingCard from './LandingCard.jsx';
 import TextInput from '../TextInput.jsx';
 import Button from '../Button.jsx';
 import '../../scss/landing.scss';
+// import API from '../../api/API';
 
 /// callBack should be passed from landing page. Used to re-render the landing page
 /// with the default landing page card, which should be the login card.
 
 function ForgotPassword({ switchCard }) {
+  // Leaving input as stateless since it should probably reset if user goes back
+  // to login.
   let input;
   const [err, setError] = useState(null);
 
   function sendResetEmail(event) {
     event.preventDefault();
-    // TODO: Make reset password request
     setError('Not Implemented');
   }
 
   return (
     <LandingCard title='Password reset' error={err}>
-      <form>
+      <form onSubmit={sendResetEmail}>
         <TextInput
           placeHolder="johndoe@email.com"
           // eslint-disable-next-line no-unused-vars
           onChange={(c) => { input = c; }}
         />
-        <Button onClick={sendResetEmail}>
+        <Button type='submit'>
           Send Reset Email
         </Button>
         <Button className="btn-link" onClick={() => switchCard('login')}>Back To Login</Button>
