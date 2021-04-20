0097a06f673b182d05df9007bc06231c8054e10d
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-04-20 18:07:19 -0400

    CRLF->LF

--- src/event/PasswordValidation.jsx
+++
@@ -1,9 +0,0 @@
-async function onInputChange(event) {
-  const formInfo = new FormData(event.target);
-  const data = Object.formEntries(formInfo);
-  if (!data.password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/gm)) {
-    throw (new Error('Your password must be at least 8 characters long, contain a lowercase letter, uppercase letter, number, and special character.'));
-  }
-}
-
-export default onInputChange;
