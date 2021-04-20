513490153797f21e5eaf4f39f16fe8d7c193b159
Elbert <etse168@gmail.com>
2021-04-20 11:27:57 -0400

    Added reset password option in user menu

--- src/components/dashboard/UserMenu.jsx
+++ src/components/dashboard/UserMenu.jsx
@@ -5,7 +5,10 @@ import PropTypes from 'prop-types';
 function UserMenu({ onLogout }) {
   return (
     <Menu>
-      <Menu.Item key="0" onClick={onLogout}>
+      <Menu.Item key="0">
+          Reset Password
+      </Menu.Item>
+      <Menu.Item key="1" onClick={onLogout}>
           Logout
       </Menu.Item>
     </Menu>
