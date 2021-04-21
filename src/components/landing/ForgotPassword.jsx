327314e62f1eba31ec3f483523e58b87a1d28ce2
Elbert <etse168@gmail.com>
2021-04-20 21:34:59 -0400

    Moved code from feature/forgot-password to this branch

--- src/components/landing/ForgotPassword.jsx
+++ src/components/landing/ForgotPassword.jsx
@@ -1,32 +1,49 @@
 import React, { useState } from 'react';
+import { notification } from 'antd';
 import PropTypes from 'prop-types';
 import LandingCard from './LandingCard.jsx';
 import TextInput from '../TextInput.jsx';
 import Button from '../Button.jsx';
 import '../../scss/landing.scss';
+import API from '../../api/API';
 
 /// callBack should be passed from landing page. Used to re-render the landing page
 /// with the default landing page card, which should be the login card.
 
 function ForgotPassword({ switchCard }) {
-  let input;
   const [err, setError] = useState(null);
+  const [input, setInput] = useState('');
 
-  function sendResetEmail(event) {
+  function isTrimmedEmpty(str) {
+    if (str.trim() === '') {
+      return true;
+    }
+    return false;
+  }
+
+  async function onSubmit(event) {
     event.preventDefault();
-    // TODO: Make reset password request
-    setError('Not Implemented');
+    try {
+      await API.passwordRecovery(input);
+      notification.success({
+        description: 'Please check your email for the password reset link.',
+      });
+      setInput('');
+      setError('');
+    } catch (e) {
+      setError(e.message);
+    }
   }
 
   return (
     <LandingCard title='Password reset' error={err}>
-      <form>
+      <form onSubmit={onSubmit}>
         <TextInput
           placeHolder="johndoe@email.com"
           // eslint-disable-next-line no-unused-vars
-          onChange={(c) => { input = c; }}
+          onChange={(c) => { setInput(c); }}
         />
-        <Button onClick={sendResetEmail}>
+        <Button disabled={isTrimmedEmpty(input)} type='submit'>
           Send Reset Email
         </Button>
         <Button className="btn-link" onClick={() => switchCard('login')}>Back To Login</Button>
