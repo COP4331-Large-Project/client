ccdd0759a91fd1fdaed0e2dca05f9aaf1ba178e4
Elbert <etse168@gmail.com>
2021-04-21 15:56:35 -0400

    Added notifications for leaving a group

--- src/components/dashboard/GroupsMenu.jsx
+++ src/components/dashboard/GroupsMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Menu, Popconfirm, notification } from 'antd';
 import PropTypes from 'prop-types';
 
 function GroupsMenu({ isOwner, groupId }) {
@@ -7,10 +7,36 @@ function GroupsMenu({ isOwner, groupId }) {
     alert(`${isOwner} for ${groupId}`);
   }
 
+  function leaveGroup() {
+    try {
+      // Make API call
+      // Update groups
+      notification.success({
+        description: 'Successfully left the group.',
+        duration: 2,
+      });
+    } catch (err) {
+      notification.error({
+        title: 'Could not delete group.',
+        description: err,
+      });
+    }
+  }
+
   return (
     <Menu>
-      <Menu.Item key="0" onClick={amITheOwner}>
-        Am I the owner?
+      <Menu.Item key="0" disabled = {isOwner}>
+        <Popconfirm
+          title='Are you sure you want to leave this group?'
+          okText="Yes"
+          cancelText="No"
+          onConfirm={leaveGroup}
+        >
+          Leave Group
+        </Popconfirm>
+      </Menu.Item>
+      <Menu.Item key="1" onClick={amITheOwner} disabled = {!isOwner}>
+        Delete Group
       </Menu.Item>
     </Menu>
   );
